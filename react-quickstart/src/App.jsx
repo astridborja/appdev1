df7ef67d7cae23b0985852eafaebdc1ac385fdaf
mahermosamalapit <astridborja@student.laverdad.edu.ph>
2024-10-15 23:05:56 +0800

    im done completing section on displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,16 +1,15 @@
 
 import './App.css';
+import Profile from './Profile';
 
 function App() {
   return (
     <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
       <h1>Welcome to my app</h1>
+      <Profile/>
       <img className="avatar" src="https://i.imgur.com/yXOvdOSs.jpg"/>
+
     </>
   );
 }
-
-
 export default App
